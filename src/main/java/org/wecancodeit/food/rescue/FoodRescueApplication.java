9ac040f28eb6f08ba354850fec8c9be742368f3a
YourGitHubName <alan@wecancodeit.org>
2018-07-21 11:21:39 -0400

    first commit

---
+++ src/main/java/org/wecancodeit/food/rescue/FoodRescueApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.food.rescue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRescueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodRescueApplication.class, args);
+	}
+}
